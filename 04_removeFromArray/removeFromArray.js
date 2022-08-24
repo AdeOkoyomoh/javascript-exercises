18ceddb075d172c0a2f2a37876a5819e01485289
Okoyomoh Adenomoh <okoyomohadenomoh@gmail.com>
2022-08-24 08:39:49 +0100

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, toBeRemoved) {
+  for (const argument of arguments) {
+    for (const element of array) {
+      if (element === argument) {
+        array.splice(array.indexOf(element), 1);
+      }
+    }
+  }
+  return array;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
