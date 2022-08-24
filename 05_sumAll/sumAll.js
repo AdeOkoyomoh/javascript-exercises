7ecbeaba0a3756db8110486bdd5ad4c60d00147e
Okoyomoh Adenomoh <okoyomohadenomoh@gmail.com>
2022-08-24 09:18:14 +0100

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(startingNumber, endingNumber) {
+  let numbersBetween = [];
+  if (startingNumber > 0 && typeof startingNumber === 'number' && endingNumber > 0 && typeof endingNumber === 'number') {
+    if (startingNumber < endingNumber) {
+      for (let i = startingNumber; i <= endingNumber; i++) {
+        numbersBetween.push(i);
+      }
+    } else {
+      for (let i = startingNumber; i >= endingNumber; i--) {
+        numbersBetween.push(i);
+      }
+    }
+  } else {
+    return 'ERROR';
+  }
+  
+  console.log(numbersBetween);
+  
+  let result = 0;
+  for (let i = 0; i < numbersBetween.length; i++) {
+    result += numbersBetween[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
